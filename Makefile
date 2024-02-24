4f1aec413f7c315288a7bfe5f8af9ea41bcf7612
goropikari <goropikari56@gmail.com>
2024-02-24 16:12:23 +0900

    custom

---
+++ Makefile
@@ -0,0 +1,15 @@
+setup:
+	nvim --headless "+Lazy! sync" +qa
+
+install:
+	rm -rf ~/.local/neovim
+	mkdir -p ~/.local/neovim
+	cd ~/.local/neovim
+	wget https://github.com/neovim/neovim/releases/download/v0.9.5/nvim.appimage
+	chmod u+x nvim.appimage
+	./nvim.appimage --appimage-extract
+	ln -sf ${HOME}/.local/neovim/squashfs-root/usr/bin/nvim ${HOME}/.local/bin/nvim
+
+clean:
+	rm -rf ~/.local/share/nvim
+	rm -rf ~/.local/state/nvim
---
+++ lua/custom/plugins/base.lua
@@ -0,0 +1,43 @@
+-- [[ Setting options ]]
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+
+-- Set highlight on search
+vim.o.hlsearch = true
+
+-- Make line numbers default
+vim.wo.number = true
+
+-- Enable mouse mode
+vim.o.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
+-- Enable break indent
+-- 折り返された行もインデントが適用して表示される
+vim.o.breakindent = true
+
+-- Save undo history
+-- vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+-- 検索時に大文字小文字を区別しない。ただし \C を付けて検索すると区別する
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+-- git の差分がある行や breakpoint の位置などを表示する列を常に表示する
+vim.wo.signcolumn = 'yes'
+
+-- Decrease update time
+-- vim.o.updatetime = 250
+-- vim.o.timeoutlen = 300
+
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect'
+
+-- NOTE: You should make sure your terminal supports this
+vim.o.termguicolors = true
