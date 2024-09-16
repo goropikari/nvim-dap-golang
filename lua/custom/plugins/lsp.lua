dcff4de0e1ea31c195223eade1fa4fea4c196e46
goropikari <goropikari56@gmail.com>
2024-09-16 09:42:38 +0900

    fix lsp BufWritePre

--- lua/custom/plugins/lsp.lua
+++ lua/custom/plugins/lsp.lua
@@ -46,24 +46,37 @@ local on_attach = function(client, bufnr)
   end, { desc = 'Format current buffer with LSP' })
 
   -- BufWritePre: 保存時に自動的に実行される処理
-  if client.supports_method("textDocument/formatting") then
-    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      buffer = bufnr,
-      callback = function()
-        vim.lsp.buf.format()
-        vim.lsp.buf.code_action({
-          context = {
-            diagnostics = {},
-            only = { "source.organizeImports" },
-          }, -- 保存時に自動的に goimports するときに必要
-          apply = true,
-        })
-      end,
-    })
+  -- https://github.com/golang/tools/blob/master/gopls/doc/vim.md#neovim-imports
+  local gopls_format = function()
+    local params = vim.lsp.util.make_range_params()
+    params.context = { only = { "source.organizeImports" } }
+    -- buf_request_sync defaults to a 1000ms timeout. Depending on your
+    -- machine and codebase, you may want longer. Add an additional
+    -- argument after params if you find that you have to write the file
+    -- twice for changes to be saved.
+    -- E.g., vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params)
+    for cid, res in pairs(result or {}) do
+      for _, r in pairs(res.result or {}) do
+        if r.edit then
+          local enc = (vim.lsp.get_client_by_id(cid) or {}).offset_encoding or "utf-16"
+          vim.lsp.util.apply_workspace_edit(r.edit, enc)
+        end
+      end
+    end
+    vim.lsp.buf.format({ async = false })
   end
+
+  vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    group = vim.api.nvim_create_augroup("lsp_formatting", { clear = true }),
+    callback = function()
+      if vim.bo.filetype == "go" then
+        gopls_format()
+      else
+        vim.lsp.buf.format({ async = false })
+      end
+    end
+  })
 
   local wk = require('which-key')
   wk.add(
